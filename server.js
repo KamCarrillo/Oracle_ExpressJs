9253858be5eee0c07e9d34885210515dc2dcc902
Kam Carrillo <92351526+KamCarrillo@users.noreply.github.com>
2024-06-03 21:13:03 -0600

    Add files via upload
    First version [database connection]

---
+++ server.js
@@ -0,0 +1,56 @@
+const port = process.env.PORT || 3000,
+    express = require('express'),
+    app = express();
+
+const oracledb = require('oracledb');
+
+async function run() {
+    let connection;
+
+    try {
+        // Connect to the database
+        connection = await oracledb.getConnection({
+            user: 'cc_proy_admin',       
+            password: 'cc',   
+            connectString: '172.17.0.2:1521/cursobd.fi.unam'
+        });
+
+        console.log('Connected to the database!');
+
+        // Execute a simple query
+        //const result = await connection.execute(`SELECT * FROM your_table`);
+        //console.log(result.rows);
+
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                // Always close the connection
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+}
+
+run();
+
+
+
+app.get("/", (req, res)=> {
+    //res.sendStatus(500)
+    //res.json({message: Error})
+    //res.download("index.js")
+    res.render("index")
+})
+
+
+
+console.log("Texto prueba")
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
